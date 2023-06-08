1fd50c476925646f2bbf034d972aac08a900ff4a
Githu77 <githubenard16@gmail.com>
2023-06-08 14:19:49 +0300

    fix 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,6 +3,7 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
+
     nargs = len(sys.argv) - 1
     if nargs != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
