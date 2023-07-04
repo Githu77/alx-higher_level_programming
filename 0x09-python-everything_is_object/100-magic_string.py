01c805c24eb56528af325c909c3645aef67174c4
Githu77 <githubenard16@gmail.com>
2023-07-04 17:02:00 +0300

    fix 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""prevents creating dynamic attributes"""
+
+
 def magic_string(l=[]):
+    """prevents creating dynamic attributes"""
     l += ["BestSchool"]
     return ", ".join(l)
