3440adb0d8ce75020688b5534151b7bc0a05dee4
Githu77 <githubenard16@gmail.com>
2023-08-16 11:56:56 +0300

    add files

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- with the hbtn_0d_tvshows database it lists genres of the show Dexter.
+SELECT tv_genres.name
+  FROM tv_shows
+  INNER JOIN tv_show_genres
+  ON tv_shows.id = tv_show_genres.show_id
+  INNER JOIN tv_genres
+  ON tv_show_genres.genre_id = tv_genres.id
+  WHERE tv_shows.title = "Dexter"
+  ORDER BY tv_genres.name;
